a5c1cc300fc2cabd4b97cb1b00e6de0aecd5a018
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-09-10 08:10:53 -0400

    set up the connection to the database.

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+// First lets Import the mongoose library
+const mongoose = require('mongoose');
+// This will Connect to the MongoDB database using the MongoDB URI provided in the environment 
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/Thomas&Friends',{useNewUrlParser: true, useUnifiedTopology: true,
+});
+// This is going to Export the connection to the database as a module
+module.exports = mongoose.connection
